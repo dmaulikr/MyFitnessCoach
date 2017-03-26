0589b957fa20d0987afa4a61358bb02c6e023150
Andrew Meng <andrewmeng@Andrews-MacBook-Pro.local>
2017-03-25 23:13:42 -0400

    Initial Commit

---
+++ MyFitnessCoach/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyFitnessCoach
+//
+//  Created by Andrew Meng on 2017-03-25.
+//  Copyright © 2017 Andrew Meng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
